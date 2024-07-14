7305197ca7b84158662750c208f4e2aa8bd4c317
Daniil <ki5896787@gmail.com>
2024-07-14 22:55:35 +0300

    Created easy weather observer

--- src/main/java/HW31/DisplayPhone.java
+++ src/main/java/HW31/DisplayPhone.java
@@ -4,6 +4,6 @@ public class DisplayPhone implements Observer{
 
     @Override
     public void update(int temperature) {
-        System.out.println("Температура сьгодні: " + temperature + " градусів за цельсієм, яка була надіслана на телефон");
+        System.out.println("Температура сьгодні: " + temperature + " градусів за цельсієм; надіслано на телефон");
     }
 }
--- src/main/java/HW31/DisplayTv.java
+++ src/main/java/HW31/DisplayTv.java
@@ -3,6 +3,6 @@ package HW31;
 public class DisplayTv implements Observer{
     @Override
     public void update(int temperature) {
-        System.out.println("Температура сьгодні: " + temperature + " градусів за цельсієм, яка була надіслана на телевізор");
+        System.out.println("Температура сьгодні: " + temperature + " градусів за цельсієм; надіслано на телевізор");
     }
 }
